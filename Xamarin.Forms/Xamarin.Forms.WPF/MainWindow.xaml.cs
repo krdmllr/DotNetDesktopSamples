534765810256e1bac15b3b06fc31fa0619410578
Konrad Mueller <k.l.mueller@outlook.com>
2020-05-21 20:08:18 +0200

    Add WPF .NET Core sample

---
+++ Xamarin.Forms/Xamarin.Forms.WPF/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Xamarin.Forms.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new FormsDesktop.App());
+        }
+    }
+}
